f68245b91c90e37b674a8846828cf843ba65e8ea
Ellie Keen <elliekeen95@gmail.com>
2018-05-01 14:53:29 +0100

    POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -2,14 +2,24 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, "Our secret session"
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player_name_1 = params[:player_name_1]
-    @player_name_2 = params[:player_name_2]
-    erb(:index)
+  post '/names' do
+    session[:player_name_1] = params[:player_name_1]
+    session[:player_name_2] = params[:player_name_2]
+    redirect '/play'
   end
+
+  get '/play' do
+    @player_1 = session[:player_name_1]
+    @player_2 = session[:player_name_2]
+    erb :play
+  end
+
 
 end

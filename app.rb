d74687015ebecf4dacd44cb6aea5e3ef541a7b57
William Powell <william.powell92@gmail.com>
2018-05-04 11:27:03 +0100

    Moved player instances into game class

--- app.rb
+++ app.rb
@@ -12,24 +12,23 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_name_1])
-    $player_2 = Player.new(params[:player_name_2])
-    $game = Game.new
+    $game = Game.new(Player.new(params[:player_name_1]),
+                     Player.new(params[:player_name_2]))
 
     redirect '/play'
   end
 
   get '/play' do
-    @player_name_1 = $player_1.name
-    @player_name_2 = $player_2.name
-    @player_hit_points_1 = $player_1.health
-    @player_hit_points_2 = $player_2.health
+    @player_name_1 = $game.player1.name
+    @player_name_2 = $game.player2.name
+    @player_hit_points_1 = $game.player1.health
+    @player_hit_points_2 = $game.player2.health
 
     erb :play
   end
 
   get '/cripplethem' do
-    $game.attack($player_2)
+    $game.attack($game.player2)
 
     erb :cripplethem
   end
